f7663e326eb908475c390e03948ee4350d329f84
Luke Bayliss <lukepbayliss@gmail.com>
2022-06-10 16:00:08 +1000

    fix method

--- server/routers/dump.router.ts
+++ server/routers/dump.router.ts
@@ -4,6 +4,6 @@ import { DumpController } from "../controllers";
 const router = Router();
 
 router.post("/api/dump", DumpController.create);
-router.post("/api/dump/:id", DumpController.fetch);
+router.get("/api/dump/:id", DumpController.fetch);
 
 export default router;
