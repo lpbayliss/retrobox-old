cce0c3620757e473e00077b219486daeb19e6126
Luke Bayliss <lukepbayliss@gmail.com>
2022-07-28 12:53:35 +1000

    create theme package

---
+++ packages/theme/src/index.ts
@@ -0,0 +1,19 @@
+
+import { extendTheme } from "@chakra-ui/react";
+import styles from "./styles";
+import semanticTokens from "./semantic-tokens";
+// import Section from "./components/section";
+// import Card from "./components/card";
+// import Heading from "./components/heading";
+
+const overrides = {
+  styles,
+  semanticTokens,
+  components: {
+    // Section,
+    // Card,
+    // Heading,
+  },
+};
+
+export default extendTheme(overrides);
---
+++ packages/theme/src/semantic-tokens.ts
@@ -0,0 +1,18 @@
+const semanticTokens = {
+  colors: {
+    canvas: {
+      default: 'gray.50',
+      _dark: 'gray.800'
+    },
+    surface: {
+      default: 'gray.100',
+      _dark: 'gray.900'
+    },
+    subtext: {
+      default: 'gray.500',
+      _dark: 'gray.500'
+    }
+  }
+};
+
+export default semanticTokens;
