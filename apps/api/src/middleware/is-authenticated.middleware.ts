7dad895442f237eab85d3a3c53484d5c07fae418
Luke Bayliss <hello@lukebayliss.com>
2022-09-09 15:49:11 +1000

    even more logging

--- apps/api/src/middleware/is-authenticated.middleware.ts
+++ apps/api/src/middleware/is-authenticated.middleware.ts
@@ -1,11 +1,17 @@
 import { NextFunction, Request, Response } from "express";
+import { default as parentLogger } from "../services/logger";
+
+const logger = parentLogger.child({ service: "auth-middleware" });
 
 const isAuthenticatedMiddleware = (
   req: Request,
   res: Response,
   next: NextFunction
 ) => {
+  logger.debug("Validating authentication");
+  logger.debug(req.isAuthenticated());
   if (req.isAuthenticated()) return next();
+  logger.info("Unauthorized access attempted", req);
   return res.status(401).send({
     title: "https://retrobox.app/probs/unauthorized",
     status: 401,
